68f1659d66eb29a0f2cd4a03825caa6802eef293
wss124 <wellyson123443@gmail.com>
2024-10-12 11:08:32 -0300

    Initial commit - Projeto Spring Boot Loja de Perfumes

---
+++ src/test/java/com/perfumaria/perfumaria/PerfumariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumaria.perfumaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
